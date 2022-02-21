8eeefef38faf25c0efd2c7b42014febb88b695ec
Serge Chvorun <elifnull@protonmail.com>
2022-02-20 22:01:44 -0500

    initial commit

---
+++ first_jsx/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <section className='todo_list'>
+        <ul>
+          <li>Lear React</li>
+          <li>Clime mt. Kilimengaro</li>
+          <li>Learn React Native</li>
+          <li>maybe do Some Next.js</li>
+          <li>Run a marathon</li>
+        </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
